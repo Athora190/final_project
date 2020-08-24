2490a2bc5bd3033e7a306930489186a457cb1466
Athora190 <jademarte7@gmail.com>
2020-08-23 20:12:47 -0400

    little fix

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-function switchFile()
-{
-   let file1  = querySelector(".storyHeader");
-   let file2 = querySelector (".storyHeader2")
-   
-    var randomNumber = Math.floor(Math.random() * 3) + 1;//creates random No. from 1 - 3
-    
-}
+// function switchFile()
+// {
+//    let file1  = querySelector(".storyHeader");
+//    let file2 = querySelector (".storyHeader2")
 
-document.getElementById("story1").innerHTML = addEventListener("click", switchFile);
+//     var randomNumber = Math.floor(Math.random() * 3) + 1;//creates random No. from 1 - 3
 
+// }
+
+// document.getElementById("story1").innerHTML = addEventListener("click", switchFile);
