5df3e5f6e46ae9b8357678dae36eb589da60bd3e
Athora190 <jademarte7@gmail.com>
2020-08-26 10:21:36 -0400

    finished

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-// function switchFile()
-// {
-//    let file1  = querySelector(".storyHeader");
-//    let file2 = querySelector (".storyHeader2")
-
-//     var randomNumber = Math.floor(Math.random() * 3) + 1;//creates random No. from 1 - 3
-
-// }
-
-// document.getElementById("story1").innerHTML = addEventListener("click", switchFile);
+let comment = document.getElementById("submit");
+function submition() {
+  alert("Thank you for your comment");
+}
+comment.addEventListener("click", submition);
